b6d8ea9cf4cb85716ddf4c07c1dad763bbe7d80d
Ahmed Elsankary <elsankary99@gmail.com>
2024-03-07 12:18:46 +0200

    Show the map

--- lib/views/pages/map/map_page.dart
+++ lib/views/pages/map/map_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 @RoutePage()
 class MapPage extends StatelessWidget {
@@ -8,26 +9,19 @@ class MapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FLutterMap(
-      options: MapOptions(
-        initialCenter: LatLng(51.509364, -0.128928),
-        initialZoom: 9.2,
+    return Scaffold(
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(31.0413861, 31.3828396),
+          initialZoom: 9.2,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+        ],
       ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-        RichAttributionWidget(
-          attributions: [
-            TextSourceAttribution(
-              'OpenStreetMap contributors',
-              onTap: () =>
-                  launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
